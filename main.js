d7033030987b1af917b0ce3f88c8c09a258226ad
KubaJakubowski <bjakubowski.dev@gmail.com>
2021-03-17 06:34:44 +0100

    chore: client/server test

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
-console.log("Test");
-const net = require("net")
-console.log(JSON.stringify(net))
+
+/*
+In the node.js intro tutorial (http://nodejs.org/), they show a basic tcp 
+server, but for some reason omit a client connecting to it.  I added an 
+example at the bottom.
+Save the following server in example.js:
+*/
+
+const net = require('net');
+
+const server = net.createServer(function(socket) {
+	socket.write('Echo server\r\n');
+
+	
+
+	server.on('error', (e)=>{
+        	console.log("Error received: ", e);
+	})
+
+	server.on('close', (data)=>{                           		console.log("Connection closed: ", data)        })                                                                                                              server.on('data', (data)=>{
+	        console.log("Data received: ", data);
+	})	
+	socket.pipe(socket);
+
+
+});
+
+server.listen(1338, '127.0.0.1');
 
